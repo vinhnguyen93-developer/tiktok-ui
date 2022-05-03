ce39d9ab8e1d073657a98d7895bc918ad43aa204
Vinhnguyen93-developer <developerthienkim@gmail.com>
2022-05-03 22:28:58 +0700

    Setup css/sass

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import GlobalStyles from './components/GlobalStyles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <GlobalStyles>
+        <App />
+      </GlobalStyles>
     </BrowserRouter>
   </React.StrictMode>
 );
