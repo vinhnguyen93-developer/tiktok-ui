eb9ec6fdcc54e3828980b0e874d2f13a8501fbf6
Vinhnguyen93-developer <developerthienkim@gmail.com>
2022-06-11 23:30:23 +0700

    [Implement] Change name file

---
+++ src/components/Button/Button.js
@@ -0,0 +1,84 @@
+import PropTypes from 'prop-types';
+import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
+
+import styles from './Button.module.scss';
+
+const cx = classNames.bind(styles);
+
+function Button({
+  to,
+  href,
+  primary = false,
+  outline = false,
+  text = false,
+  disabled = false,
+  rounded = false,
+  small = false,
+  large = false,
+  children,
+  className,
+  leftIcon,
+  rightIcon,
+  onClick,
+  ...passProps
+}) {
+  let Comp = 'button';
+  const classes = cx('wrapper', {
+    [className]: className,
+    primary,
+    outline,
+    text,
+    disabled,
+    rounded,
+    small,
+    large,
+  });
+
+  const props = {
+    onClick,
+    ...passProps,
+  };
+
+  // Remove event listener when button is disabled
+  if (disabled) {
+    Object.keys(props).forEach((key) => {
+      if (key.startsWith('on') && typeof props[key] === 'function') {
+        delete props[key];
+      }
+    });
+  }
+
+  if (to) {
+    props.to = to;
+    Comp = Link;
+  } else if (href) {
+    props.href = href;
+    Comp = 'a';
+  }
+
+  return (
+    <Comp className={classes} {...props}>
+      {leftIcon && <span className={cx('icon')}>{leftIcon}</span>}
+      <span className={cx('title')}>{children}</span>
+    </Comp>
+  );
+}
+
+Button.propTypes = {
+  to: PropTypes.string,
+  href: PropTypes.string,
+  primary: PropTypes.bool,
+  outline: PropTypes.bool,
+  text: PropTypes.bool,
+  disabled: PropTypes.bool,
+  rounded: PropTypes.bool,
+  small: PropTypes.bool,
+  large: PropTypes.bool,
+  className: PropTypes.string,
+  leftIcon: PropTypes.node,
+  rightIcon: PropTypes.node,
+  onClick: PropTypes.func,
+};
+
+export default Button;
