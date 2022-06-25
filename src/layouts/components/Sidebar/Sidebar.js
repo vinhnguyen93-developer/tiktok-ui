7bf1c5a97de7c52c1c8efc14221a7f9f7fc85dbe
Vinhnguyen93-developer <developerthienkim@gmail.com>
2022-06-25 23:32:07 +0700

    [Implement] UI Sidebar #1

---
+++ src/layouts/components/Sidebar/Sidebar.js
@@ -0,0 +1,44 @@
+import classNames from 'classnames/bind';
+import Menu, { MenuItem } from './Menu';
+import config from '~/config';
+import {
+  HomeIcon,
+  HomeActiveIcon,
+  UserGroupIcon,
+  UserGroupActiveIcon,
+  LiveIcon,
+  LiveActiveIcon,
+} from '~/components/Icons';
+
+import styles from './Sidebar.module.scss';
+
+const cx = classNames.bind(styles);
+
+function Sidebar() {
+  return (
+    <aside className={cx('wrapper')}>
+      <Menu>
+        <MenuItem
+          title="For You"
+          to={config.routes.home}
+          icon={<HomeIcon />}
+          activeIcon={<HomeActiveIcon />}
+        />
+        <MenuItem
+          title="Following"
+          to={config.routes.following}
+          icon={<UserGroupIcon />}
+          activeIcon={<UserGroupActiveIcon />}
+        />
+        <MenuItem
+          title="LIVE"
+          to={config.routes.live}
+          icon={<LiveIcon />}
+          activeIcon={<LiveActiveIcon />}
+        />
+      </Menu>
+    </aside>
+  );
+}
+
+export default Sidebar;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -8,11 +8,13 @@ import Home from '~/pages/Home';
 import Following from '~/pages/Following';
 import Profile from '~/pages/Profile';
 import Upload from '~/pages/Upload';
+import Live from '~/pages/Live';
 
 // Public routes
 const publicRoute = [
   { path: config.routes.home, component: Home },
   { path: config.routes.following, component: Following },
+  { path: config.routes.live, component: Live },
   { path: config.routes.profile, component: Profile },
   { path: config.routes.upload, component: Upload, layout: HeaderOnly },
   // { path: config.routes.search, component: Search, layout: null },
